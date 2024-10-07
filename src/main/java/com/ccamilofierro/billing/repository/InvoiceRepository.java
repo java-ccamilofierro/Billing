171af72c1dfbe843c8b2866e5f9603c0442d3b67
Cristian Fierro <ccamilofierro@gmail.com>
2024-10-07 00:26:36 -0500

    feat: add basic Invoice service functionality, implement JpaRepository

--- src/main/java/com/ccamilofierro/billing/repository/InvoiceRepository.java
+++ src/main/java/com/ccamilofierro/billing/repository/InvoiceRepository.java
@@ -1,5 +1,10 @@
 package com.ccamilofierro.billing.repository;
 
-public class InvoiceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ccamilofierro.billing.model.Invoice;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
 
 }
--- src/main/java/com/ccamilofierro/billing/service/InvoiceService.java
+++ src/main/java/com/ccamilofierro/billing/service/InvoiceService.java
@@ -1,5 +1,13 @@
 package com.ccamilofierro.billing.service;
 
-public class InvoiceService {
+import java.util.List;
 
+import com.ccamilofierro.billing.model.Invoice;
+
+public interface InvoiceService {
+    List<Invoice> getAllInvoices();
+    Invoice getInvoiceById(Long id);
+    Invoice createInvoice(Invoice invoice);
+    Invoice updateInvoice(Invoice invoice);
+    void deleteInvoice(Long id);
 }
